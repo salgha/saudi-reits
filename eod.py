a4b68d45e617b677979c1b437af2179b9d66370a
Saleh Alghamdi <salgha@me.com>
2023-04-02 07:23:42 +0300

    set up actions workflow

--- eod.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-import yfinance as yf
-from datetime import datetime
---
+++ eod_applet.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import yfinance as yf
+from datetime import datetime
+
+def get_eod_price(ticker):
+    t = '^' + ticker if ticker == 'trti' else ticker
+    df = yf.Ticker(t+'.SR').history(period='1d', interval='1d')['Close'].reset_index()
+    df['Date'] = pd.to_datetime(df['Date']).dt.floor('d').dt.tz_localize(None)
+    df = df.rename(columns={'Date':'date', 'Close':ticker}).set_index('date')
+    return df
+
+tickers = ['4330', '4331', '4332', 
+           '4333', '4334', '4335', 
+           '4336', '4337', '4338', 
+           '4339', '4340', '4342', 
+           '4344', '4345', '4346', 
+           '4347', '4348']
+
+df = pd.read_csv('data/pdata.csv', index_col=0)
+df.index = pd.to_datetime(df.index)
+
+df_eod = get_eod_price('trti')
+
+if df_eod.iloc[-1:].index.values not in df.index.values:
+    for ticker in tickers:
+        df_eod_aux = get_eod_price(ticker)
+        df_eod = df_eod.merge(df_eod_aux, on='date', how='left')
+        
+    df = pd.concat([df, df_eod])
+    
+    df.to_csv('data/pdata.csv', index=False)
